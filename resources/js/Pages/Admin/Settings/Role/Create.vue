e18af5f750d3edef49d63d9b13aad907c95fd979
luis <luis@pop-os.localdomain>
2023-01-03 23:26:09 -0300

    feat: select all role

--- resources/js/Pages/Admin/Settings/Role/Create.vue
+++ resources/js/Pages/Admin/Settings/Role/Create.vue
@@ -33,17 +33,31 @@ const form = useForm({
     permissions: []
 })
 
+function checkAll(items) {
+    const formPermissions = form.permissions
+    if (formPermissions.length === 0) {
+        form.permissions = items.map(item => item.id)
+        return;
+    }
+    if (items.map(item => item.id).every(item => formPermissions.includes(item))) {
+        form.permissions = formPermissions.filter(item => !items.map(item => item.id).includes(item))
+        return;
+    }
+
+    form.permissions = [...formPermissions, ...items.map(item => item.id)]
+}
+
 </script>
 <template>
     <AuthenticatedLayout>
-        <div class="tw-flex tw-items-center tw-justify-between">
+        <div class="tw-flex tw-flex-col tw-gap-4 md:tw-items-center md:tw-justify-between md:tw-gap-0 md:tw-flex-row">
             <h4 class="tw-text-gray-600 tw-text-2xl tw-font-bold">Cadastrar grupo de permissões</h4>
             <breadcrumb :items="itemsBread"/>
         </div>
-        <card class="tw-mt-8">
+        <card class="tw-mt-4 md:tw-mt-8">
             <title-card title="Cadastrar grupos" subtitle="Use os campos abaixos para cadastrar grupos." :actions="actionsTitle"/>
             <form class="tw-mt-4" @submit.prevent="form.post(route('admin.settings.roles.store'))">
-                <div class="tw-flex tw-gap-4">
+                <div class="tw-flex tw-gap-4 tw-flex-col md:tw-flex-row">
                     <div class="tw-w-full">
                         <label for="name" class="tw-block tw-text-sm tw-font-medium tw-text-gray-400">Nome</label>
                         <q-input id="name" class="tw-mt-1" dense outlined v-model="form.name" :error="!!form.errors.name" :error-message="form.errors.name"/>
@@ -53,10 +67,11 @@ const form = useForm({
                         <q-input id="description" class="tw-mt-1" dense outlined v-model="form.description"/>
                     </div>
                 </div>
-                <div class="tw-mt-8 tw-grid tw-grid-cols-3 tw-gap-6">
+                <div class="tw-mt-8 tw-grid tw-grid-cols-1 tw-gap-4 md:tw-grid md:tw-grid-cols-3 md:tw-gap-6">
                     <div v-for="(item, key) in permissions" :key="item.id">
-                        <div class="tw-w-full tw-bg-gray-300 tw-py-2 tw-px-4 tw-capitalize tw-text-[18px] tw-text-gray-700">
+                        <div class="tw-w-full tw-bg-gray-300 tw-py-2 tw-px-4 tw-capitalize tw-text-[18px] tw-text-gray-700 tw-flex tw-justify-between tw-items-center">
                             {{key}}
+                            <button type="button" class="tw-text-sm tw-text-indigo-500" @click="checkAll(item)">Marca todos</button>
                         </div>
                         <div class="tw-w-full tw-py-2 tw-capitalize tw-text-md tw-text-gray-700 tw-grid tw-grid-cols-2 tw-gap-2">
                             <div v-for="permission in item" :key="permission.id" class="tw-flex tw-items-center tw-gap-2 tw-text-[16px]">
