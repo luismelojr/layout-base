8201786214f0d1caa0b9952d582c151f7cfae88d
luis <junimhs1996@gmail.com>
2023-01-11 23:25:59 -0300

    feat: create mobile menu

--- resources/js/Components/Navigation.vue
+++ resources/js/Components/Navigation.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { Popover, PopoverButton, PopoverPanel } from '@headlessui/vue'
+import {Disclosure, DisclosureButton, Popover, PopoverButton, PopoverPanel} from '@headlessui/vue'
+import ApplicationLogo from "@/Components/ApplicationLogo.vue";
 const mobileMenuOpen = ref(false)
 const links = [
     {
@@ -75,35 +76,45 @@ function toggleMenu() {
                 </PopoverPanel>
             </Popover>
         </ul>
-        <div class="tw-w-full md:tw-hidden" :class="{'tw-mt-4': mobileMenuOpen}">
+        <div class="tw-w-full md:tw-hidden tw-mt-4">
             <button class="tw-cursor-pointer" @click="toggleMenu">
                 <i class='bx bx-menu tw-text-[24px]'></i>
             </button>
         </div>
-    </div>
-    <div v-if="mobileMenuOpen" class="tw-bg-white tw-w-full tw-px-[24px] tw-py-4 tw-border-t tw-mt-2 tw-shadow">
-        <ul class="tw-w-full tw-items-start tw-gap-4 -tw-ml-1 tw-flex tw-flex-col">
-            <Popover class="tw-relative" v-for="link in links" :key="link.name" v-can="link.can" :class="{'tw-flex tw-items-center tw-gap-3': !!link.children}">
-                <PopoverButton>
-                    <div v-if="!!link.children" class="tw-text-[15px] tw-flex tw-items-center tw-gap-2 tw-cursor-pointer" :class="{'tw-text-indigo-500': isActive(link.routeActive), 'tw-text-gray-400': !isActive(link.routeActive)}">
-                        <i :class="link.icon" class="tw-text-[15px]"></i>
-                        <span>{{ link.name }}</span>
-                        <i v-if="!!link.children" class='bx bx-chevron-down tw-text-[16px] tw-text-inherit'></i>
-                    </div>
-                    <Link v-else :href="link.href" class="tw-text-[15px] tw-flex tw-items-center tw-gap-2" :class="{'tw-text-indigo-500': isActive(link.routeActive), 'tw-text-gray-400': !isActive(link.routeActive)}">
-                        <i :class="link.icon" class="tw-text-[15px]"></i>
-                        <span>{{ link.name }}</span>
-                        <i v-if="!!link.children" class='bx bx-chevron-down tw-text-[16px] tw-text-inherit'></i>
-                    </Link>
-                </PopoverButton>
 
-                <PopoverPanel v-if="!!link.children" class="tw-absolute tw-z-10 tw-top-9 tw-bg-white tw-shadow-md tw-w-full tw-py-1">
-                    <div class="tw-flex tw-flex-col">
-                        <Link v-for="(item, index) in link.children" :key="index" :href="item.href" v-can="item.can" class="hover:tw-bg-gray-100 tw-w-full tw-py-2 tw-px-4 tw-text-gray-400">{{item.name}}</Link>
+        <q-layout v-if="mobileMenuOpen">
+            <q-drawer v-model="mobileMenuOpen" side="left" :width="300" :content-css="{'min-height': '100vh'}" :overlay="true" :persistent="true">
+                <q-scroll-area class="tw-h-full tw-flex tw-flex-col tw-justify-between">
+                    <div class="tw-flex tw-flex-col tw-gap-4 tw-p-4">
+                        <div class="tw-border-b tw-pb-4">
+                            <ApplicationLogo class="tw-text-black tw-h-8 tw-w-8"/>
+                        </div>
+                        <ul class="tw-flex tw-flex-col tw-gap-4">
+                            <li v-for="link in links" :key="link.name" v-can="link.can" :class="{'tw-flex tw-items-center tw-gap-3': !!link.children}">
+                                <div v-if="!!link.children" class="tw-text-[15px] tw-cursor-pointer" :class="{'tw-text-indigo-500': isActive(link.routeActive), 'tw-text-gray-400': !isActive(link.routeActive)}">
+                                    <div class="tw-flex tw-items-center tw-gap-2">
+                                        <i :class="link.icon" class="tw-text-[15px]"></i>
+                                        <span>{{ link.name }}</span>
+                                    </div>
+                                    <div class="tw-mt-3 tw-ml-2">
+                                        <ul>
+                                            <li v-for="(item, index) in link.children" :key="index" :class="{'tw-flex tw-items-center tw-gap-3': !!item.children}">
+                                                <Link :href="item.href" v-can="item.can" class="hover:tw-bg-gray-100 tw-w-full tw-py-2 tw-px-4 tw-text-gray-400">--- {{item.name}}</Link>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <Link v-else :href="link.href" class="tw-text-[15px] tw-flex tw-items-center tw-gap-2" :class="{'tw-text-indigo-500': isActive(link.routeActive), 'tw-text-gray-400': !isActive(link.routeActive)}">
+                                    <i :class="link.icon" class="tw-text-[15px]"></i>
+                                    <span>{{ link.name }}</span>
+                                    <i v-if="!!link.children" class='bx bx-chevron-down tw-text-[16px] tw-text-inherit'></i>
+                                </Link>
+                            </li>
+                        </ul>
                     </div>
-                </PopoverPanel>
-            </Popover>
-        </ul>
+                </q-scroll-area>
+            </q-drawer>
+        </q-layout>
     </div>
 </div>
 </template>
